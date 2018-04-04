8b3a68384fad2285efc467cf1beae1ad1543e9fb
Dani <danijn9@gmail.com>
2018-04-04 15:20:24 +0200

    improvement
    convert to function.

--- check_nfs.sh
+++ check_nfs.sh
@@ -11,24 +11,29 @@
 mount -a
 
 list=`cat /etc/fstab  |grep "^1" |awk '{print $2}'`
-for i in $list
-do
+function check_nfs_mountpoints (list){
 
-#       echo $i
-        ls $i > /dev/null 2>&1
-        out_stat=$?
-        if [ $out_stat -eq 0 ]
-        then
-                echo "All is working OK!!"
-        else
-                echo "The mount point $i has failed, we have to apply a remount"
-                logger -t check_nfs_mountpoints -i "The mount point $i has failed, we have to apply a remount"
-                umount $i
-        fi
-done
+  for i in $list
+  do
 
-#If there have been failures in the mount points in the for, they have been disassembled and we re-apply a mount -a so that they are assembled
+  #       echo $i
+          ls $i > /dev/null 2>&1
+          out_stat=$?
+          if [ $out_stat -eq 0 ]
+          then
+                  echo "All is working OK!!"
+          else
+                  echo "The mount point $i has failed, we have to apply a remount"
+                  logger -t check_nfs_mountpoints -i "The mount point $i has failed, we have to apply a remount"
+                  umount $i
+          fi
+  done
 
-mount -a
+  #If there have been failures in the mount points in the for, they have been disassembled and we re-apply a mount -a so that they are assembled
+
+  mount -a
+
+}
+check_nfs_mountpoints($list)
 
 echo "End of script!"
